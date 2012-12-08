c68182779462d65a85f676fd8cb56b5b9db1634f
Michael Allen <michaeldfallen@gmail.com>
2012-12-08 14:33:18 +0000

    Initial commit.

---
+++ app/assets/javascripts/i18n/da.js
@@ -0,0 +1,12 @@
+/** Messages for Danish (dansk)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Emilkris33
+ */
+
+var I18n = {
+	on_leave_page: "Du kan miste dine ændringer",
+	loading: "Indlæser..."
+};
+
---
+++ app/assets/javascripts/i18n/ksh.js
@@ -0,0 +1,12 @@
+/** Messages for Colognian (Ripoarisch)
+ *  Exported from translatewiki.net
+ *
+ * Translators:
+ *  - Purodha
+ */
+
+var I18n = {
+	on_leave_page: "Ding Änderonge künnte verschött jonn",
+	loading: "Aam Laade\x26nbsp;\x26hellip;"
+};
+
